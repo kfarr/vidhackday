9a38f2689b8adfe6266838b6b6e3081d18a2e927
Kieran Farr <kieran.farr@gmail.com>
2011-10-01 15:47:18 -0700

    Adding movies sync management commands

--- movie/models.py
+++ movie/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Movie(models.Model):
 	RT_movie_id = models.CharField(max_length=50) # Movie ID in Rotten Tomatoes
 	title = models.CharField(max_length=255)
-	poster_detail = models.URLField()
+	poster_detailed = models.URLField()
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
