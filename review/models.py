770dfec5c21ccc0fe8e522f69788b19bb32c450a
Kieran Farr <kieran.farr@gmail.com>
2011-10-01 16:16:01 -0700

    Adds movie import to review model

--- review/models.py
+++ review/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from movie.models import Movie
 
 class Review(models.Model):
 	title = models.CharField(max_length=100)
